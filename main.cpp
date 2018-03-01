719fc08021b87d57b57eacbd82032dae97f8f3c5
Rey Almaraz <ChrisRey@wlan-t-195-63-dhcp.CSUChico.EDU>
2018-03-01 11:03:25 -0800

    GuessgameDoorC

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Christopher Rey Almaraz
 #include "SecretDoor/SecretDoor.h"
 #include <iostream>
 
@@ -6,6 +7,21 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int GameTrials;
+  int Ws=0;
+  
+  cout<<"How many times would you like to play.\n";
+  cin>>GameTrials;
+
+  for(int i=0;i<GameTrials;i++)
+  {	game.newGame();
+ 	game.guessDoorC();
+	game.guessDoorC();
+	if(game.isWinner())
+	{
+		Ws++;
+	}
+  } 
+  cout<<"you won "<< Ws<< " times.\n";
   return 0;
 }
